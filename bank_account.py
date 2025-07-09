f6abdf2d49eb86f9468aee20a316bc4d4dd1c6c2
oreopengguin <xz.zhang21@gmail.com>
2025-07-09 16:51:35 -0400

    Added more tests, do strings, and changed from error raising to returnig

--- bank_account.py
+++ bank_account.py
@@ -1,28 +1,58 @@
-import numpy
-
-
 class BankAccount:
-    def __init__(self, balance, name, num):
-        if not isinstance(balance, float) and not isinstance(balance, int):
+    def __init__(self, balance: int | float, name: str, num: int):
+        """
+        Initializes the bank account with the users balance, name, and account number
+        """
+        if (
+            not isinstance(balance, float)
+            and not isinstance(balance, int)
+            and balance > 0
+        ):
             raise TypeError("Balance must be a valid amount of money")
+            # return False
         if not isinstance(name, str):
             raise TypeError("Name must be a string")
+            # return False
         if not isinstance(num, int) and num > 0:
             raise TypeError("Account number must be a positive integer")
+            # return False
 
         self.balance = balance
         self.name = name
         self.num = num
+        # return True
 
     def withdraw(self, amnt):
+        """
+        Withdraws a passed amount of money by subtracting it from the balance
+        @param amnt the amount of money to subtract
+        @ret True if money is successfully withdrawed, False if it can't be 
+        """
         if not (isinstance(amnt, float) or isinstance(amnt, int)) or amnt < 0:
-            raise TypeError("Withdraw amount must be a valid amount of money")
+            # raise TypeError("Withdraw amount must be a valid amount of money")
+            print("Withdraw amount must be a valid amount of money")
+            return False
+        if self.balance - amnt < 0:
+            # raise ValueError("Insufficient balance")
+            print ("Insufficient balance")
+            return False
         self.balance -= amnt
+        return True
 
     def deposit(self, amnt):
+        """
+        Deposits a passed amount of money by adding it to the balance
+        @param amnt the amount of money to add
+        @ret True if money is successfully deposited, False if it can't be
+        """
         if not (isinstance(amnt, float) or isinstance(amnt, int)) or amnt < 0:
-            raise TypeError("Deposit amount must be a valid amount of money")
+            # raise TypeError("Deposit amount must be a valid amount of money")
+            return False
         self.balance += amnt
+        return True
 
     def display(self):
+        """
+        Displays the account owner's name and their balance
+        """
         return self.name + f"'s account has a balance of ${self.balance}"
--- test_bank_account.py
+++ test_bank_account.py
@@ -5,29 +5,40 @@ from bank_account import BankAccount
 class TestBankAccount(unittest.TestCase):
     def test_withdraw(self):
         self.EricAcnt = BankAccount(100.00, "Eric", 1)
-        self.EricAcnt.withdraw(1000.00)
-        self.assertAlmostEqual(self.EricAcnt.balance, -900)
+        self.EricAcnt.withdraw(10.00)
+        self.assertAlmostEqual(self.EricAcnt.balance, 90)
+        self.assertNotAlmostEqual(self.EricAcnt.balance, 32)
         self.EricAcnt.withdraw(40)
-        self.assertAlmostEqual(self.EricAcnt.balance, -940)
+        self.assertAlmostEqual(self.EricAcnt.balance, 50)
+        self.assertNotAlmostEqual(self.EricAcnt.balance, 49)
+        self.assertEqual(self.EricAcnt.withdraw(-100000.00), False)
+        self.assertEqual(self.EricAcnt.withdraw("meow"), False)
+        """
         with self.assertRaises(TypeError):
             self.EricAcnt.withdraw(-1000000.00)
         with self.assertRaises(TypeError):
             self.EricAcnt.withdraw("meow")
+        """
     
     def test_deposit(self):
         self.IsaAcnt = BankAccount(2.73, "Isa", 2)
         self.IsaAcnt.deposit(2.49)
         self.assertAlmostEqual(self.IsaAcnt.balance, 5.22)
+        self.assertNotAlmostEqual(self.IsaAcnt.balance, 5.23)
         self.IsaAcnt.deposit(3)
         self.assertAlmostEqual(self.IsaAcnt.balance, 8.22)
-        with self.assertRaises(TypeError):
-            self.IsaAcnt.deposit(-1000)
-        with self.assertRaises(TypeError):
-            self.IsaAcnt.deposit("meow")
+        self.assertNotAlmostEqual(self.IsaAcnt.balance, 8.21)
+        self.assertEqual(self.IsaAcnt.deposit(-1000), False)
+        self.assertEqual(self.IsaAcnt.deposit("meow"), False)
+        # with self.assertRaises(TypeError):
+        #     self.IsaAcnt.deposit(-1000)
+        # with self.assertRaises(TypeError):
+        #     self.IsaAcnt.deposit("meow")
     
     def test_display(self):
         self.EricAcnt = BankAccount(-940, "Eric", 1)
         self.IsaAcnt = BankAccount(8.22, "Isa", 2)
         self.assertEqual(self.EricAcnt.display(), "Eric's account has a balance of $-940")
         self.assertEqual(self.IsaAcnt.display(), "Isa's account has a balance of $8.22")
-        
+        self.assertNotEqual(self.EricAcnt.display(), "meow meow meow")
+        self.assertNotEqual(self.IsaAcnt.display(), "meow meow meow")
